92aa547bdf60d909c245fbd5f1fd02a4dcb18743
Andrei <an2rei-84@yandex.ru>
2024-06-12 23:47:12 +0500

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,18 @@
 import masks
 
+
 def mask_account_card(args: str) -> str:
+    """Функция маскировки карты или счета"""
     data_account = str(args)
+    new_number = int(args[-16:])
     if "Счет" in data_account:
-        return f"Счет {masks.get_mask_account(args)}"
+        return f"Счет {masks.get_mask_account(new_number)}"
     else:
-        data_card = data_account.replace(data_account[-16:],"")
-        return f"{data_card[0:]}{masks.mask_card_number(args[-16:])}"
-
-print(mask_account_card('Visa Platinum 7000 7922 8960 6361'))
+        data_card = data_account.replace(data_account[-16:], "")
+        return f"{data_card[0:]}{masks.mask_card_number(new_number)}"
 
 
-
-def get_data(arg: str) -> str:
-    data_ = str(arg)
+def get_data(args: str) -> str:
+    """Функция определения даты"""
+    data_ = str(args)
     return f"{data_[8:10]}.{data_[5:7]}.{data_[:4]}"
-
-
-
-

a5d8fa6a4624d47562fd3d863d0e77080fe639e3
Andrei <an2rei-84@yandex.ru>
2024-06-18 19:28:42 +0500

    add  two new functions

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,15 @@
-def filter_by_state(dict_list: list, state="EXECUTED") -> list:
+
+
+def filter_by_state(dicts_list: list, state="EXECUTED") -> list:
     """Функция возвращает новый список, содержащий только те словари,
     у которых ключ 'state'
     содержит переданное в функцию значение"""
-    filtered_list = [i for i in dict_list if i.get("state") == state]
+    filtered_list = [i for i in dicts_list if i.get("state") == state]
     return filtered_list
 
 
-def sort_by_date(info: list, date=True) -> list:
+def sort_by_date(transaction_info: list, is_date=True) -> list:
     """Функция сортировки списка словарей по дате"""
-    sorted_date = sorted(info, key=lambda x: x["date"], reverse=True)
-    return sorted_date
+    if is_date == "True":
+        return sorted(transaction_info, key=lambda x: x["date"], reverse=True)
+    return sorted(transaction_info, key=lambda x: x["date"], reverse=False)

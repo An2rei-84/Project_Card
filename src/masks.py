e7d3a7ba6045a6c34f4563c798cc43d33a996e20
Andrei <an2rei-84@yandex.ru>
2024-07-18 18:18:13 +0500

    add logs

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,22 @@
+import logging
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
 def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера карты"""
+    logger.info("Маскируем номер карты")
     card = str(card_number)
     return f"{card[:4]} {card[4:6]}** **** {card[-4:]}"
 
 
 def get_mask_account(mask_account: int) -> str:
     """Функция маскировки номера счета"""
+    logger.info("Маскируем номер счета")
     numb = str(mask_account)
     return f"**{numb[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("../logs/utils.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def get_operations_list(path):
@@ -6,14 +14,18 @@ def get_operations_list(path):
        с данными о финансовых транзакциях.
        Если файл пустой, содержит не список или не найден,
        функция возвращает пустой список."""
+    logger.info("Формируем список словарей транзакций")
     try:
         with open(path, 'r', encoding='UTF-8') as file:
             data = json.load(file)
     except FileNotFoundError:
+        logger.warning("Файл не найден")
         return []
     except json.JSONDecodeError:
+        logger.warning("Ошибка в файле")
         return []
     if not isinstance(data, list):
+        logger.warning("Файл не является списком")
         return []
     return data
 
